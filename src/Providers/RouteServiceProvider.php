22433b3e160a44f3e8c29062fb34847ae68138da
Bob Bloom <bob.bloom@lasallesoftware.ca>
2023-04-15 21:43:44 -0400

    Fix the big ass problem with RouteServiceProvider.php. #27

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -13,28 +13,47 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * The path to your application's "home" route.
      *
-     * Typically, users are redirected here after authentication.
+     * This is used by Laravel authentication to redirect users after login.
      *
      * @var string
      */
     public const HOME = '/home';
+ 
 
     /**
-     * Define your route model bindings, pattern filters, and other route configuration.
+     * Define your route model bindings, pattern filters, etc.
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        if (env('LASALLE_APP_NAME') == "adminbackendapp") {
+
+            RateLimiter::for('api', function (Request $request) {
+                return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            });
+        }
+
+        // L8 Upgrade: Automatic Controller Namespace Prefixing
+        // (https://laravel.com/docs/8.x/upgrade, scroll down)
+        // the shipped App\Providers\RouteServiceProvider's namespace property defults to null
+        // I am going to comment it out instead of deleting it for reference only
+        if (env('LASALLE_APP_NAME') == "adminbackendapp") {
+
+            $this->routes(function () {
+                Route::prefix('api')
+                    ->middleware('api')
+                    ->namespace($this->namespace)
+                    //->group(base_path('routes/api.php'));
+                    ->group(__DIR__.'/../../routes/api.php')
+                ;            
+            });
+        }
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->namespace($this->namespace)
+                //->group(base_path('routes/web.php'));
+                ->group(__DIR__.'/../../routes/web.php')
+            ;
         });
     }
 }
